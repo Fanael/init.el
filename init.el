73172f23ba8895fa034a1d57b81fc3cef7bc8eb0
Fanael Linithien <fanael4@gmail.com>
2014-02-13 20:35:53 +0100

    Use fallback font only on Windows.

--- init.el
+++ init.el
@@ -215,13 +215,13 @@ line mode."
                              :inherit 'font-lock-preprocessor-face)))))
 
 (defun init-el-setup-colors ()
-  (cond
-   ((find-font (font-spec :name "Consolas"))
-    (set-face-attribute 'default nil :family "Consolas" :height 100))
-   ((find-font (font-spec :name "Monospace"))
+  (if (eq system-type 'windows-nt)
+      (cond
+       ((find-font (font-spec :name "Consolas"))
+        (set-face-attribute 'default nil :family "Consolas" :height 100))
+       ((find-font (font-spec :name "Lucida Console"))
+        (set-face-attribute 'default nil :family "Lucida Console" :height 100)))
     (set-face-attribute 'default nil :family "Monospace" :height 102))
-   ((find-font (font-spec :name "Lucida Console"))
-    (set-face-attribute 'default nil :family "Lucida Console" :height 100)))
   (let ((theme 'colorsarenice-dark))
     (load-theme theme t)
     ;; Without this hook X11 has problems setting the fucking cursor color.
