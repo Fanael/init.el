2cce1c99d41d0e468cbf95c4fcac2731f8d869a9
Fanael Linithien <fanael4@gmail.com>
2014-09-08 18:20:03 +0200

    Reenable global-relative-line-numbers-mode.

--- init.el
+++ init.el
@@ -414,7 +414,8 @@ line mode."
         (lambda (offset)
           (if (= offset 0)
               "=>"
-            (format "%2d" (abs offset))))))
+            (format "%2d" (abs offset)))))
+  (global-relative-line-numbers-mode))
 
 (defun init-el-setup-indentation ()
   (setq-default indent-tabs-mode nil
@@ -461,7 +462,6 @@ line mode."
   (add-hook 'org-shiftleft-final-hook #'windmove-left)
   (add-hook 'org-shiftdown-final-hook #'windmove-down)
   (add-hook 'org-shiftright-final-hook #'windmove-right)
-  (global-set-key (kbd "<f5> l") #'relative-line-numbers-mode)
   (global-set-key (kbd "<f5> i") #'rainbow-identifiers-mode)
   (global-set-key (kbd "<f5> r") #'helm-recentf)
   (global-set-key [f7] #'compile)
