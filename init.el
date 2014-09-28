19d9ba6c7880d502a07746fa43c4c6c08a57c8f6
Fanael Linithien <fanael4@gmail.com>
2014-09-28 10:30:15 +0200

    Disable package sig checking on Windows for now.

--- init.el
+++ init.el
@@ -186,6 +186,8 @@
 (defun init-el-setup-package-archives ()
   (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
                            ("gnu" . "http://elpa.gnu.org/packages/")))
+  (when (eq system-type 'windows-nt)
+    (setq package-check-signature nil))
   (package-initialize))
 
 (defun init-el-install-required-packages ()
