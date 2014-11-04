da250b1265a1be2128300a9852958a2df2812b75
Fanael Linithien <fanael4@gmail.com>
2014-11-04 17:57:55 +0100

    Add a key binding for pp-macroexpand-all.

--- init.el
+++ init.el
@@ -469,6 +469,7 @@
   (global-set-key (kbd "C-c l") #'relative-line-numbers-mode)
   (global-set-key (kbd "C-c r") #'rainbow-identifiers-mode)
   (global-set-key (kbd "C-c b") #'highlight-blocks-now)
+  (global-set-key (kbd "C-c m") #'pp-macroexpand-all)
   (init-el-with-eval-after-load helm
     (define-key helm-map "\t" #'helm-execute-persistent-action)
     (define-key helm-map (kbd "C-z") #'helm-select-action)))
