7dbfd8befd20fa7b84f6ab8343a717ef266d628c
Fanael Linithien <fanael4@gmail.com>
2018-10-13 19:53:28 +0200

    Ensure the init loads even in Emacsen build with windowing support

--- init.el
+++ init.el
@@ -60,7 +60,8 @@
 
 ;;; Disable useless GUI stuff
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
 (when (fboundp 'horizontal-scroll-bar-mode)
   (horizontal-scroll-bar-mode -1))
 (menu-bar-mode -1)
